c8a8e280fbeaca319f16889f73ea236259c1012c
Norman Breau <norman@nbsolutions.ca>
2021-03-09 23:43:50 -0400

    tests for stacktraces

--- spec/NBSError.spec.ts
+++ spec/NBSError.spec.ts
@@ -10,4 +10,17 @@ describe('NBSError', () => {
         let e: NBSError = new NBSError('foo');
         expect(e.name).toBe('NBSError');
     });
+
+    it('error contains stacktrace', () => {
+        let e: NBSError = new NBSError('foo');
+        expect(e.stack).toBe('NBSError: foo\n' +
+            '    at Object.<anonymous> (/development/nbsolutions/error/spec/NBSError.spec.ts:15:27)\n' +
+            '    at Object.asyncJestTest (/development/nbsolutions/error/node_modules/jest-jasmine2/build/jasmineAsyncInstall.js:106:37)\n' +
+            '    at /development/nbsolutions/error/node_modules/jest-jasmine2/build/queueRunner.js:45:12\n' +
+            '    at new Promise (<anonymous>)\n' +
+            '    at mapper (/development/nbsolutions/error/node_modules/jest-jasmine2/build/queueRunner.js:28:19)\n' +
+            '    at /development/nbsolutions/error/node_modules/jest-jasmine2/build/queueRunner.js:75:41\n' +
+            '    at processTicksAndRejections (internal/process/task_queues.js:93:5)'
+        );
+    });
 });
