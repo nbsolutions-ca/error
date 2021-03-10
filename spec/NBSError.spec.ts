638120aa78c32458485078aa7e77e00042442f4e
Norman Breau <norman@nbsolutions.ca>
2021-03-09 23:50:54 -0400

    NBSError workaround

--- spec/NBSError.spec.ts
+++ spec/NBSError.spec.ts
@@ -1,26 +1,33 @@
 import { NBSError } from "../src/NBSError";
 
 describe('NBSError', () => {
+    let e: NBSError = null;
+
+    beforeEach(() => {
+        e = new NBSError('foo');
+    });
+
     it('constructor name should be NBSError', () => {
-        let e: NBSError = new NBSError('foo');
         expect(e.constructor.name).toBe('NBSError');
     });
 
     it('error name should be NBSError', () => {
-        let e: NBSError = new NBSError('foo');
         expect(e.name).toBe('NBSError');
     });
 
     it('error contains stacktrace', () => {
-        let e: NBSError = new NBSError('foo');
         expect(e.stack).toBe('NBSError: foo\n' +
-            '    at Object.<anonymous> (/development/nbsolutions/error/spec/NBSError.spec.ts:15:27)\n' +
-            '    at Object.asyncJestTest (/development/nbsolutions/error/node_modules/jest-jasmine2/build/jasmineAsyncInstall.js:106:37)\n' +
+            '    at Object.<anonymous> (/development/nbsolutions/error/spec/NBSError.spec.ts:7:13)\n' +
+            '    at Object.asyncJestLifecycle (/development/nbsolutions/error/node_modules/jest-jasmine2/build/jasmineAsyncInstall.js:58:37)\n' +
             '    at /development/nbsolutions/error/node_modules/jest-jasmine2/build/queueRunner.js:45:12\n' +
             '    at new Promise (<anonymous>)\n' +
             '    at mapper (/development/nbsolutions/error/node_modules/jest-jasmine2/build/queueRunner.js:28:19)\n' +
             '    at /development/nbsolutions/error/node_modules/jest-jasmine2/build/queueRunner.js:75:41\n' +
             '    at processTicksAndRejections (internal/process/task_queues.js:93:5)'
         );
+    });
+
+    it('NBSError is not an Error instance', () => {
+        expect(e instanceof Error).toBe(false);
     });
 });
--- src/NBSError.ts
+++ src/NBSError.ts
@@ -1,4 +1,12 @@
 
+/*
+ * We use the Error interface instead of extending the Error object.
+ * This is because if you extend Error, it will forcefully overwrite the constructor
+ * losing access to it's name, which causes a wide range of negative side effects.
+ * 
+ * Unfortunately, this means you cannot use instanceof Error checks, but instanceof NBSError
+ * checks should still work so that shouldn't be a big deal...
+ */
 export class NBSError implements Error {
     public name: string;
     public message: string;
